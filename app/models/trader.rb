f8763746a2cb62563bbe371adad4db3c0727c66a
sevenInches <seven@seveninches.local>
2017-04-09 22:52:56 +0800

    m

--- app/models/trader.rb
+++ app/models/trader.rb
@@ -3,6 +3,7 @@ class Trader < ApplicationRecord
   has_many :orders
 
   has_one :trader_detail
+
   belongs_to :deliver
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,13 @@ class User < ApplicationRecord
   has_many :activities
   has_many :appraisals
   has_many :orders
+
+  def sex
+    case gender
+      when 1 then '男'
+      when 2 then '女'
+      else
+        '未知'
+    end
+  end
 end
